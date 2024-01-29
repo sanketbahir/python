8f0979c0e354bfc0fd574551c5f82d741a89e887
sanketbahir <sanketbahir2016@gmail.com>
2024-01-29 13:39:54 +0530

    files added

---
+++ if_strStatement.py
@@ -0,0 +1,4 @@
+txt = "hi my name is sanket and am working with DataVerze"
+if "DataVerze" in txt:
+    print("yes sanket is working with dataverze")
+    
---
+++ str_length.py
@@ -0,0 +1,2 @@
+a = "hi my name is sanket and am learning pytho"
+print(len(a))
